f370b1529861be72c627052c4e981bc187f1f8ea
Дмитрий Вокуев <vokuev1996@inbox.ru>
2021-12-04 18:30:42 +0300

    Добавьте файлы проекта.

---
+++ 1_Task7_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Task7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            CalcCub(a);
+            Console.ReadKey();
+        }
+        static void CalcCub(int a)
+        {
+            int S = 6 * a * a;
+            int V = a * a * a;
+            Console.WriteLine("Площадь поверхности куба равна {0}, объём куба равен {1}", S, V);
+            
+        }
+    }
+}
+    
+
